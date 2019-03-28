f8be8df30230a3d0116fa1635b030209c05378df
tt <1508278816@qq.com>
2019-03-28 16:23:08 +0800

    5.5

---
+++ src/components/ebook/EbookReader.vue
@@ -0,0 +1,156 @@
+<template>
+<div class="ebook-reader">
+  <div id="read"></div>
+</div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import { mapActions } from 'vuex';
+  import { ebookMixin } from '../../utils/mixin'
+  import Epub from 'epubjs';
+  import {
+    getFontFamily,
+    saveFontFamily,
+    getFontSize,
+    saveFontSize,
+    getTheme,
+    saveTheme,
+    getLocation
+  } from '../../utils/localStorage'
+
+  global.eub = Epub;
+
+  export default {
+    mixins: [ebookMixin],
+    methods:{
+      prevPage() {
+        if(this.rendition){
+          this.rendition.prev().then(() =>{
+            this.refreshLocation()
+          })
+          this.hideTitleAndMenu()
+        }
+      },
+      nextPage() {
+        if(this.rendition){
+          this.rendition.next().then(() =>{
+            this.refreshLocation()
+          })
+          this.hideTitleAndMenu()
+        }
+      },
+      toggleTitleAndMenu() {
+        if(this.menuVisible){
+          this.setSettingVisible(-1)
+          this.setFontFamilyVisible(false)
+        }
+          this.$store.dispatch('setMenuVisible',!this.menuVisible)
+      },
+      hideTitleAndMenu() {
+        //this.$store.dispatch('setMenuVisible',false)
+        this.setMenuVisible(false)
+        this.setSettingVisible(-1)
+        this.setFontFamilyVisible(false)
+      },
+      initFontSize(){
+        let fontSize = getFontSize(this.fileName)
+          if(!fontSize){
+            saveFontSize(this.fileName, this.defaultFontSize)
+          } else {
+            this.rendition.themes.fontSize(fontSize)
+            this.setDefaultFontSize(fontSize)
+        }
+      },
+      initFontFamily(){
+        let font = getFontFamily(this.fileName)
+          if(!font){
+            saveFontFamily(this.fileName, this.defaultFontFamily)
+          } else {
+            this.rendition.themes.font(font)
+            this.setDefaultFontFamily(font)
+        }
+      },
+      initTheme(){
+        let defaultTheme = getTheme(this.fileName)
+        if(!defaultTheme){
+          defaultTheme = this.themeList[0].name
+          saveTheme(this.fileName, defaultTheme)
+        }
+        this.setDefaultTheme(defaultTheme)
+        this.themeList.forEach(theme => {
+          this.rendition.themes.register(theme.name, theme.style)
+        })
+        this.rendition.themes.select(defaultTheme)
+      },
+      initRendition(){
+        this.rendition = this.book.renderTo('read', {
+          width: window.innerWidth,
+          height: window.innerHeight,
+          method: 'default'
+        })
+        const location = getLocation(this.fileName)
+        this.display(location, () =>{
+          this.initTheme()
+          this.initFontSize()
+          this.initFontFamily()
+          this.initGlobalStyle()
+        })
+        this.rendition.hooks.content.register(contents => {
+          Promise.all([
+            contents.addStylesheet(`${process.env.VUE_APP_RES_URL}/fonts/daysOne.css`),
+            contents.addStylesheet(`${process.env.VUE_APP_RES_URL}/fonts/cabin.css`),
+            contents.addStylesheet(`${process.env.VUE_APP_RES_URL}/fonts/montserrat.css`),
+            contents.addStylesheet(`${process.env.VUE_APP_RES_URL}/fonts/tangerine.css`)
+          ]).then(() => {
+
+          })
+        })
+      },
+      initGestrue(){
+        this.rendition.on('touchstart', event => {
+          this.touchStartX = event.changedTouches[0].clientX
+          this.touchStartTime = event.timeStamp
+        })
+        this.rendition.on('touchend', event => {
+          const offsetX = event.changedTouches[0].clientX - this.touchStartX
+          const time = event.timeStamp - this.touchStartTime
+
+          if(time < 500 && offsetX > 40){
+            this.prevPage()
+          }
+          else if(time < 500 && offsetX < -40) {
+            this.nextPage()
+          }
+          else {
+            this.toggleTitleAndMenu()
+          }
+          event.preventDefault()
+          event.stopPropagation()
+        })
+      },
+      initEpub() {
+        const url = process.env.VUE_APP_RES_URL + '/epub/' + this.fileName + '.epub'
+        this.book = new Epub(url)
+        this.setCurrentBook(this.book)
+        this.initRendition()
+        this.initGestrue()
+        this.book.ready.then(() => {
+          return this.book.locations.generate(750 * (window.innerWidth / 375) * (getFontSize(this.fileName) / 16))
+        }).then(locations => {
+          this.setBookAvailable(true)
+          this.refreshLocation()
+        })
+      }
+    },
+    mounted() {
+      this.$store.dispatch('setFileName', this.$route.params.fileName.split('|').join('/')).then(() => {
+          this.initEpub()
+      })
+    }
+  }
+</script>
+
+<style lang='scss' rel='stylesheet/scss' scoped>
+  @import "../../assets/styles/global";
+</style>
---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/ebook',
+      component: () => import('./views/ebook/index.vue'),
+      children: [
+          {
+            path: ':fileName',
+            component: () => import('./components/ebook/EbookReader.vue')
+          }
+      ]
+    }
+  ]
+})
